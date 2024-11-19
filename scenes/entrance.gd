b53b358c1447f7ba7d20431a0ab47f45b1e6ed6a
Santiago Vergara <santiagovrlr@gmail.com>
2024-11-18 22:03:34 -0600

    Door open

--- scenes/entrance.gd
+++ scenes/entrance.gd
@@ -30,3 +30,37 @@ func _on_floor_0_trigger_body_entered(body):
 func _on_floor_1_trigger_body_entered(body):
 	if body is Player:
 		$Player.current_floor = 1
+
+# XD
+func open_door():
+	$TileMapLayer.set_cell(Vector2i(-2, -104), 0, Vector2i(2, 0))
+	$TileMapLayer.set_cell(Vector2i(-1, -104), 0, Vector2i(3, 0))
+	$TileMapLayer.set_cell(Vector2i(-2, -103), 0, Vector2i(2, 1))
+	$TileMapLayer.set_cell(Vector2i(-1, -103), 0, Vector2i(3, 1))
+	$TileMapLayer.set_cell(Vector2i(-2, -102), 0, Vector2i(2, 2))
+	$TileMapLayer.set_cell(Vector2i(-1, -102), 0, Vector2i(3, 1))
+	$TileMapLayer.set_cell(Vector2i(-2, -101), 0, Vector2i(2, 2))
+	$TileMapLayer.set_cell(Vector2i(-1, -101), 0, Vector2i(3, 1))
+	$TileMapLayer.set_cell(Vector2i(-2, -100), 0, Vector2i(2, 2))
+	$TileMapLayer.set_cell(Vector2i(-1, -100), 0, Vector2i(3, 1))
+	$TileMapLayer.set_cell(Vector2i(-2, -99), 0, Vector2i(2, 3))
+	$TileMapLayer.set_cell(Vector2i(-1, -99), 1, Vector2i(0, 4))
+	
+	$TileMapLayer.set_cell(Vector2i(0, -104), 0, Vector2i(3, 0), 1)
+	$TileMapLayer.set_cell(Vector2i(1, -104), 0, Vector2i(2, 0), 1)
+	$TileMapLayer.set_cell(Vector2i(0, -103), 0, Vector2i(3, 1))
+	$TileMapLayer.set_cell(Vector2i(1, -103), 0, Vector2i(2, 1), 1)
+	$TileMapLayer.set_cell(Vector2i(0, -102), 0, Vector2i(3, 1))
+	$TileMapLayer.set_cell(Vector2i(1, -102), 0, Vector2i(2, 2), 1)
+	$TileMapLayer.set_cell(Vector2i(0, -101), 0, Vector2i(3, 1))
+	$TileMapLayer.set_cell(Vector2i(1, -101), 0, Vector2i(2, 2), 1)
+	$TileMapLayer.set_cell(Vector2i(0, -100), 0, Vector2i(3, 1))
+	$TileMapLayer.set_cell(Vector2i(1, -100), 0, Vector2i(2, 2), 1)
+	$TileMapLayer.set_cell(Vector2i(0, -99), 1, Vector2i(0, 4))
+	$TileMapLayer.set_cell(Vector2i(1, -99), 0, Vector2i(2, 3), 1)
+
+func _on_plank_locks_unlocked():
+	open_door()
+
+func _on_church_area_trigger_area_entered(area):
+	print("going to church")
